1976b90e263877d70e84e4e8c360b48354a5bc6a
karneaud <kendall.arneaud@gmail.com>
2023-05-21 20:36:38 +0000

    Add mouseover styles

--- components/Card.vue
+++ components/Card.vue
@@ -2,7 +2,7 @@
   <div class="card">
     <header class="header"><h3>{{ project.name }}</h3></header>
     <div class="picture-container">
-      <picture :style="`background-image:url(${project.covers.max_808});`" :title="project.name">
+      <picture class="grayscale-background" :style="`background-image:url(${project.covers.max_808});`" :title="project.name">
     <!-- <source
         :srcset="`${project.covers['115']}  115w, ${project.covers['808']} 808w,  ${project.covers.original} 1200w`">
     <img :src="project.covers.max_808" :alt="project.name"> -->
