d210692c355ffc26f5aa78197621b64f83ac3c2b
karneaud <17814013-karneaud@users.noreply.replit.com>
2023-05-21 17:12:25 +0000

    Initial commit

---
+++ App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="grid">
+    <Card v-for="project in filteredProjects" :key="project.id" :project="project" />
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue';
+import { projects } from './data.json';
+export default {
+  components: {
+    Card
+  },
+  data() {
+    return {
+      projects: []
+    };
+  },
+  computed: {
+    filteredProjects() {
+      return this.projects.filter(project => {
+        return project.fields.includes('Game Design');
+      });
+    }
+  },
+  mounted() {
+   this.projects = projects 
+//     fetch(process.env.DATA_JSON_URL,{
+//   method: 'GET',
+//   mode: 'cors',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Access-Control-Allow-Origin': '*' // Allow requests from any origin
+//   }
+// })
+//       .then(response => response.json() )
+//       .then(data => {
+//         this.projects = projects //data.projects;
+//       })
+//       .catch(error => {
+      //  console.error('Error fetching data:', error);
+      //});
+  }
+};
+</script>
---
+++ index.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import eruda from "eruda";
+
+eruda.init()
+
+createApp(App).mount('#main')
